3e78fd0b9317b93422b35b41a8755955cb00069b
Leonard304 <167664878+Leonard304@users.noreply.github.com>
2025-04-09 17:12:44 +0800

    first commit

---
+++ public/script.js
@@ -0,0 +1,108 @@
+var type = "";
+
+function showEnroll(){
+    document.querySelector(".enrollment").classList.toggle("show");
+}
+
+function showGrade(){
+    document.querySelector(".grades").classList.toggle("show");
+}
+function showBill(){
+    document.querySelector(".billing").classList.toggle("show");
+}
+function showStuds(){
+    document.querySelector(".manStud").classList.toggle("adShow");
+}
+function showEmps(){
+    document.querySelector(".manFS").classList.toggle("adShow");
+}
+function showUstype(){
+    document.querySelector(".utype").classList.toggle("showUtype");
+}
+function showStudType(){
+    document.querySelector(".chooseType").classList.toggle("showUtype");
+}
+var typeUser = "";
+function highlightuserType(type){
+    document.querySelector(".pickStudent").classList.remove("active");
+    document.querySelector(".pickTeacher").classList.remove("active");
+    if(type == "Student"){
+        typeUser = "Student";
+        document.querySelector(".pickStudent").classList.add("active");
+    }else if(type == "Teacher"){
+        typeUser = "Teacher";
+        document.querySelector(".pickTeacher").classList.add("active");
+        document.querySelector(".pickJunior").classList.remove("actives");
+        document.querySelector(".pickSenior").classList.remove("actives");
+    }
+}
+function highlightStudType(type) {
+    document.querySelector(".pickJunior").classList.remove("actives");
+    document.querySelector(".pickSenior").classList.remove("actives");
+    if(type == "Junior High"){
+        document.querySelector(".pickStudent").classList.add("active");
+        document.querySelector(".pickJunior").classList.add("actives");
+        document.querySelector(".pickTeacher").classList.remove("active");
+    }else if(type == "Senior High"){
+        document.querySelector(".pickStudent").classList.add("active");
+        document.querySelector(".pickSenior").classList.add("actives");
+        document.querySelector(".pickTeacher").classList.remove("active");
+    }
+}
+
+function submit(){
+    const firstname = document.getElementById("firstname").value;
+    const lastname = document.getElementById("lastname").value;
+    const email = document.getElementById("email").value;
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    
+    const body = JSON.stringify({
+        typeUser: typeUser,
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        username: username,
+        password: password
+    });
+    
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4 && xhr.status == 200){
+            if(this.responseText == "OK"){
+                location.reload();
+            }
+        }
+    }
+    
+    xhr.open("POST", "http://localhost:3000/register");
+    xhr.setRequestHeader('content-type', 'application/json');
+
+    xhr.send(body);
+}
+function studentLogIn(){
+    const username = document.getElementById("studUsername").value;
+    const password = document.getElementById("studPassword").value;
+    const body = JSON.stringify({
+        username: username,
+        password: password
+    });
+    
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4 && xhr.status == 200){
+            if(this.responseText == "OK"){
+                location.pathname("/studentDashboard.ejs");
+            }else if(this.responseText == "Error"){
+                document.querySelector(".sPaneError").classList.toggle("showSPaneError");
+            }
+        }
+    }
+    
+    xhr.open("POST", "http://localhost:3000/studLogin");
+    xhr.setRequestHeader('content-type', 'application/json');
+
+    xhr.send(body);
+}
+
+
---
+++ web.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const mysql = require("mysql2");
+const indexRouter = require("./routes/index.js");
+const cors = require("cors");
+
+const web = express();
+web.set("view engine", "ejs");
+web.use(express.json());
+web.use(cors());
+
+
+web.locals.message = "";
+// web.use(express.urlencoded({extended:true}));
+
+web.use('/', indexRouter);
+
+//Database Connection
+const con = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'student_info_system'
+}).promise();
+
+web.post('/register', (req, res) => {
+    const reqBody = req.body;
+
+    const typeUser = reqBody.typeUser;
+    const name = reqBody.firstname + " " +reqBody.lastname;
+    const email = reqBody.email;
+    const username = reqBody.username;
+    const password = reqBody.password;
+
+    async function insertRegister(){
+        // Change ra Query !!!!!
+        if(typeUser == "Student"){
+            const [result] = await con.query(`INSERT INTO students (Student_Name, Email, Username, Password) VALUES (?, ?, ?, ?)`, [name, email, username, password]);
+            
+            if(result){
+                web.locals.message = "OK pre Success";
+                res.send("OK").status(200);
+            }else{
+                web.locals.message = "Error pre";
+                res.send("Error").status(200);
+            }
+    
+            console.log(result.insertId);
+        }else if(typeUser == "Teacher"){
+            const [result] = await con.query(`INSERT INTO teacher (Teacher_Name, Email, Username, Password) VALUES (?, ?, ?, ?)`, [name, email, username, password]);
+            
+            if(result){
+                web.locals.message = "OK pre Success";
+                res.send("OK").status(200);
+            }else{
+                web.locals.message = "Error pre";
+                res.send("Error").status(200);
+            }
+    
+            console.log(result.insertId);
+        }
+    }
+
+    insertRegister();
+});
+web.post('/studLogin', (req, res) =>{
+    const reqBody = req.body;
+    const username = reqBody.username;
+    const password = reqBody.password;
+    async function studEnter(){
+        // Change ra Query !!!!!
+        con.query(`SELECT * FROM students WHERE Username = ? AND Password = ?`,[username,password],(err,result,fields) =>{
+            if(result.length > 0){
+                res.send("OK").status(200);
+                web.locals.message("OK pre");
+            }else{
+                res.send("Error").status(200);
+                web.locals.message("Error");
+            }
+        });
+    }
+    studEnter();
+});
+
+web.use(express.static("public"));
+
+web.listen(3000, () =>{
+    console.log('Express is running on port 3000');
+
+});
+
